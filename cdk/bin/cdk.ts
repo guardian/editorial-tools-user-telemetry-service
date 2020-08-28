89aa4cf375966e1c6b463e591a9d71906f20850c
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2020-08-28 15:25:38 +0100

    Update dependencies; add synth command, and add generated file to .gitignore

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { TelemetryStack } from '../lib/cdk-stack';
+import { TelemetryStack } from '../lib/telemetry-stack';
 
 const app = new cdk.App();
 new TelemetryStack(app, 'TelemetryStack');
