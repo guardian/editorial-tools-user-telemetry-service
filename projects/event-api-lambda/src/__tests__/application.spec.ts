4a7eb3374bfbfae2ec2339efbd6e84fbf2967919
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2020-09-02 14:33:25 +0100

    Add lambda code scaffold and test

--- projects/event-api-lambda/src/__tests__/application.spec.ts
+++ projects/event-api-lambda/src/__tests__/application.spec.ts
@@ -1,18 +1,19 @@
-import { createApp } from '../application'
-import chai from 'chai'
-import chaiHttp from 'chai-http'
+import { createApp } from "../application";
+import chai from "chai";
+import chaiHttp from "chai-http";
 
-chai.use(chaiHttp)
-chai.should()
+chai.use(chaiHttp);
+chai.should();
 
-describe('Event API lambda', () => {
-    const testApp = createApp()
-    it('should return 200 from healthcheck', done => {
-        chai.request(testApp)
-            .get('/healthcheck')
-            .end((_, res) => {
-                expect(res.status).toBe(200)
-                done()
-            })
-    });
-})
+describe("Event API lambda", () => {
+  const testApp = createApp();
+  it("should return 200 from healthcheck", (done) => {
+    chai
+      .request(testApp)
+      .get("/healthcheck")
+      .end((_, res) => {
+        expect(res.status).toBe(200);
+        done();
+      });
+  });
+});
