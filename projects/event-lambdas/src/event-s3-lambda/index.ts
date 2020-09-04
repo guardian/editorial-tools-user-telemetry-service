88eaef77887040db293e1769e320434c25c4abb2
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2020-09-04 18:49:57 +0100

    Add logging for each state

--- projects/event-lambdas/src/event-s3-lambda/index.ts
+++ projects/event-lambdas/src/event-s3-lambda/index.ts
@@ -16,31 +16,29 @@ export const lambdaHandler = async (
   const Key = event?.Records[0].s3.object.key;
 
   if (!Bucket || !Key) {
+    const message = "S3 data not present in received event";
+    console.error(message, event);
     return {
       statusCode: 400,
-      body: JSON.stringify(
-        createErrorResponse(`S3 data not present in received event`, event)
-      ),
+      body: JSON.stringify(createErrorResponse(message, event)),
     };
   }
 
   const maybeEvents = await getEventsFromS3File(Key);
 
   if (maybeEvents.error) {
+    const message = `Invalid data in file with key ${Key}`;
+    console.error(message, maybeEvents.error);
     return {
       statusCode: 400,
-      body: JSON.stringify(
-        createErrorResponse(
-          `Invalid data in file with key ${Key}`,
-          maybeEvents.error
-        )
-      ),
+      body: JSON.stringify(createErrorResponse(message, maybeEvents.error)),
     };
   }
 
   await putEventsToKinesisStream(maybeEvents.value);
 
   const message = `Written ${maybeEvents.value.length} events to Kinesis`;
+  console.log(message);
   return {
     statusCode: 201,
     body: JSON.stringify(createOkResponse(message)),
