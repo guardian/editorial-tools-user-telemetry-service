a1b5508e1cc069bd08797b5cce96886fe3bf52b7
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2020-09-04 12:12:36 +0100

    Add tests to ensure file is written and well-formed

--- projects/event-api-lambda/src/__tests__/application.spec.ts
+++ projects/event-api-lambda/src/__tests__/application.spec.ts
@@ -2,6 +2,9 @@ import { createApp } from "../application";
 import chai from "chai";
 import chaiHttp from "chai-http";
 
+import { telemetryBucketName } from "../constants";
+import { s3 } from "../aws";
+
 chai.use(chaiHttp);
 chai.should();
 
@@ -87,7 +90,7 @@ describe("Event API lambda", () => {
         {
           stage: "PROD",
           type: "USER_ACTION_1",
-          value: 1
+          value: 1,
         },
       ];
 
@@ -104,7 +107,7 @@ describe("Event API lambda", () => {
           },
         ],
         message: "Incorrect event format",
-        status: "error"
+        status: "error",
       };
 
       return chai
@@ -135,6 +138,38 @@ describe("Event API lambda", () => {
         .then((res) => {
           expect(res.status).toBe(204);
         });
+    });
+
+    it("should write well-formed requests to S3 as NDJSON, and return the file key for easy retrieval", async () => {
+      const request = [
+        {
+          app: "example-app",
+          stage: "PROD",
+          type: "USER_ACTION_1",
+          value: 1,
+          eventTime: "2020-09-03T11:39:42.936Z",
+        },
+      ];
+
+      const res = await chai.request(testApp).post("/event").send(request);
+
+      const expectedResponse = {
+        message: "data/2020-09-03/2020-09-03T17:34:37.839Z-mock-uuid",
+        status: "ok",
+      };
+
+      expect(res.status).toBe(201);
+      expect(res.body).toEqual(expectedResponse);
+
+      const params = {
+        Bucket: telemetryBucketName,
+        Key: res.body.message,
+      };
+      const writtenFile = await s3.getObject(params).promise();
+
+      // We expect the file to contain our request as NDJSON
+      const expectedFileContents = `${JSON.stringify(request[0])}\n`;
+      expect(writtenFile.Body?.toString()).toBe(expectedFileContents);
     });
   });
 });
