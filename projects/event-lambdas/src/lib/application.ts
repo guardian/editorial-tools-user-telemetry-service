f49df87455fdb29f92f5adfc38380f6d477b0fd1
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2020-09-16 10:31:53 +0100

    Remove console statements

--- projects/event-lambdas/src/lib/application.ts
+++ projects/event-lambdas/src/lib/application.ts
@@ -8,10 +8,8 @@ export const createApp = (): express.Application => {
   const app = express();
 
   app.use((req, res, next) => {
-    console.log({ url: req.get('origin') });
     const host = req.get('origin') || '';
     if (host.endsWith(".gutools.co.uk") || host.endsWith(".dev-gutools.co.uk")) {
-      console.log("Adding headers");
       res.header("Access-Control-Allow-Headers", "Content-Type");
       res.header("Access-Control-Allow-Origin", req.get('origin'));
       res.header("Access-Control-Allow-Methods", "OPTIONS,POST,GET");
