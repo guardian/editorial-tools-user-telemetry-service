ae0e488c5c614d85189154368f97cc2022c14e33
Sam Hession <samhession@live.co.uk>
2022-03-15 12:04:14 +0000

    feat: test deplyo

--- projects/user-telemetry-client/release.config.cjs
+++ projects/user-telemetry-client/release.config.cjs
@@ -1,21 +1,21 @@
 module.exports = {
     debug: true,
     branches: [
-      { name: "main" },
-      {
-        name: "beta",
-        prerelease: true,
-      },
+        { name: "main" },
+        {
+            name: "beta",
+            prerelease: true,
+        },
     ],
     plugins: [
-      [
-        "@semantic-release/commit-analyzer",
-        {
-          preset: "conventionalcommits",
-        },
-      ],
-      "@semantic-release/release-notes-generator",
-      "@semantic-release/npm",
-      "@semantic-release/github"
+        [
+            "@semantic-release/commit-analyzer",
+            {
+                preset: "conventionalcommits",
+            },
+        ],
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/npm",
+        "@semantic-release/github"
     ],
-  };
+};
