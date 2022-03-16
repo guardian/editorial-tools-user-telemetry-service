156ed1663f036ae635c1b5297c17151598cdbca8
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2022-03-16 16:38:45 +0000

    Add test

---
+++ cdk/lib/telemetry-stack.spec.ts
@@ -0,0 +1,15 @@
+import "@aws-cdk/assert/jest";
+import { SynthUtils } from "@aws-cdk/assert";
+import { App } from "@aws-cdk/core";
+import { TelemetryStack } from "./telemetry-stack";
+
+describe("The telemetry stack", () => {
+    it("matches the snapshot", () => {
+        const app = new App();
+        const stack = new TelemetryStack(app, "telemetry", {
+            stack: "flexible",
+        });
+
+        expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+    })
+})
