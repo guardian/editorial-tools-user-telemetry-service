82a223049abdbab2bbd284d5d7f21edce6e525a2
Robert Kenny <kenoir@gmail.com>
2023-05-15 16:54:05 +0100

    Add HMAC authentication to event-api-lambda

--- projects/event-lambdas/src/lib/authentication.ts
+++ projects/event-lambdas/src/lib/authentication.ts
@@ -5,7 +5,10 @@ import {
 } from "@guardian/pan-domain-node";
 import { Request, Response } from "express";
 
-import { pandaSettingsKey } from "./constants";
+import { createHmac } from "crypto";
+
+// TODO: We may wish to configure multiple keys for different clients
+import { pandaSettingsKey, hmacSecretKey, hmacAllowedDateOffsetInMillis } from "./constants";
 import { applyErrorResponse } from "../event-api-lambda/util";
 
 export const panda = new PanDomainAuthentication(
@@ -16,30 +19,70 @@ export const panda = new PanDomainAuthentication(
   guardianValidation
 );
 
+function isHMACValid(date: string, path: string, requestToken: string) {
+  const hmac = createHmac('sha256', hmacSecretKey);
+  const content = date + '\n' + path;
+  hmac.update(content, "utf8");
+
+  return "HMAC " + hmac.digest('base64');
+}
+
+function isDateValid(requestDate: string) {
+  const parsedDate = Date.parse(requestDate);
+  if(parsedDate === NaN) {
+    return false;
+  }
+
+  const currentDate = Date.parse(new Date().toUTCString());
+  const dateDelta = Math.abs(parsedDate - currentDate);
+
+  return dateDelta > hmacAllowedDateOffsetInMillis
+}
+
 export async function authenticated(
   panda: PanDomainAuthentication,
   req: Request,
   res: Response,
   handler: () => Promise<void>
 ): Promise<void> {
-  const cookie = Array.isArray(req.headers["cookie"])
-    ? req.headers["cookie"][0]
-    : req.headers["cookie"] || "";
+  if(
+      req.headers["x-gu-tools-hmac-token"] !== undefined &&
+      req.headers["x-gu-tools-hmac-date"] !== undefined) {
+    // Check if we are doing HMAC Authentication, if so check the token & date given
 
-  if (!cookie) {
-    const message =
-      "No pan-domain authentication cookie present in the request";
-    applyErrorResponse(res, 403, message);
-    return;
-  }
+    const requestToken = req.headers["x-gu-tools-hmac-token"] as string;
+    const requestDate = req.headers["x-gu-tools-hmac-date"] as string;
 
-  return panda.verify(cookie).then(({ status }) => {
-    switch (status) {
-      case AuthenticationStatus.AUTHORISED:
-        return handler();
-      default:
-        applyErrorResponse(res, 403, "Invalid credentials");
-        return;
+    if(isDateValid(requestDate) && isHMACValid(requestDate, req.path, requestToken)) {
+      return handler();
+    } else {
+      const message =
+          "Invalid HMAC authenticated request!";
+      applyErrorResponse(res, 403, message);
+      return;
     }
-  });
+  } else {
+    // No HMAC authentication headers so assume we need to do regular panda auth
+
+    const cookie = Array.isArray(req.headers["cookie"])
+        ? req.headers["cookie"][0]
+        : req.headers["cookie"] || "";
+
+    if (!cookie) {
+      const message =
+          "No pan-domain authentication cookie present in the request";
+      applyErrorResponse(res, 403, message);
+      return;
+    }
+
+    return panda.verify(cookie).then(({status}) => {
+      switch (status) {
+        case AuthenticationStatus.AUTHORISED:
+          return handler();
+        default:
+          applyErrorResponse(res, 403, "Invalid credentials");
+          return;
+      }
+    });
+  }
 }
--- projects/event-lambdas/src/lib/constants.ts
+++ projects/event-lambdas/src/lib/constants.ts
@@ -4,3 +4,8 @@ export const telemetryStreamName =
   process.env.TELEMETRY_STREAM_NAME || "user-telemetry-stream";
 export const pandaSettingsKey =
   process.env.PANDA_SETTINGS_KEY || "local.dev-gutools.co.uk.settings.public";
+// TODO: The app should not have a default value, and should fail if one is not set
+export const hmacSecretKey =
+    process.env.HMAC_SECRET_KEY || "changeme";
+export const hmacAllowedDateOffsetInMillis =
+    process.env.HMAC_DATE_OFFSET_IN_MILLIS || 5000;
