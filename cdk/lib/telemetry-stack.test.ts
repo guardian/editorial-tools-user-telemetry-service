e4d81d3b7ec4a5e35a87394305af101050c03ac0
Robert Kenny <kenoir@gmail.com>
2023-05-17 15:59:31 +0100

    context key is required as ci in teamcity fails

--- cdk/lib/telemetry-stack.test.ts
+++ cdk/lib/telemetry-stack.test.ts
@@ -1,10 +1,16 @@
+import { ContextKeys } from '@guardian/cdk/lib/constants';
 import { App } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import { TelemetryStack } from './telemetry-stack';
 
 describe('The telemetry stack', () => {
 	it('matches the snapshot', () => {
-		const app = new App();
+		const app = new App({
+			context: {
+				[ContextKeys.REPOSITORY_URL]:
+					'https://github.com/guardian/editorial-tools-user-telemetry-service',
+			},
+		});
 		const stack = new TelemetryStack(app, 'telemetry', {
 			stack: 'flexible',
 			stage: 'CODE',
